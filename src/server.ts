682a6986afd46fdd100a14241be52ba5bb78fc15
Esha <shuminnahar8di@gmail.com>
2026-05-17 08:07:07 +0600

    Update server.ts to enhance request handling and modify .gitignore to include package-lock.json

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
-import express, { type Application } from "express"
+import express, { text, type Application, type Request, type Response } from "express"
 const app : Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended: true})) // will accept nested data
+
+
+app.get('/', (req:Request, res:Response) => {
+  res.status(200).json({
+    message:"this is root",
+    "author":"esha"
+  })
+})
+
+app.post("/user", async(req:Request, res:Response)=>{
+  console.log(req.body)
+
 })
 
 app.listen(port, () => {
