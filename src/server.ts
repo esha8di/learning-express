0cdce28ecc3a979c07e24a5f5da0fabd6f652932
esha8di <shuminnahar8di@gmail.com>
2026-05-18 09:04:28 +0600

    done

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express, { text, type Application, type Request, type Response } from "ex
 const app : Application = express()
 const port = 3000
 
+import {Pool} from "pg"
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended: true})) // will accept nested data
 
-
+const pool= new Pool({
+  connectionString:"postgresql://neondb_owner:npg_dUWIvE51OSmq@ep-misty-leaf-aqbki0w4.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
+})
 app.get('/', (req:Request, res:Response) => {
   res.status(200).json({
     message:"this is root",
@@ -16,7 +19,6 @@ app.get('/', (req:Request, res:Response) => {
 
 app.post("/user", async(req:Request, res:Response)=>{
   console.log(req.body)
-
 })
 
 app.listen(port, () => {
