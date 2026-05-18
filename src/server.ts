104c3d9aa852b80bf7cf4452657f8a43c7a734ea
esha8di <shuminnahar8di@gmail.com>
2026-05-18 17:50:48 +0600

    hello vps

--- src/server.ts
+++ src/server.ts
@@ -81,11 +81,31 @@ app.get("/api/user/:id", async (req: Request, res: Response) => {
       return res.status(404).json({ message: "user not found" });
     }
 
-    res.status(200).json({ message: "user retrieved successfully", data: result.rows[0] });
+    res.status(200).json({status:true, message: "user retrieved successfully", data: result.rows[0] });
   } catch (err) {
     res.status(500).json({ message: "error fetching user", error: err });
   }
 });
+
+app.put("/api/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, email, password, age } = req.body;
+  try{
+    const result =await pool.query(
+      `UPDATE 
+      users set 
+      name =COALESCE($1, name), email = COALESCE($2, email), password = COALESCE($3, password), age = COALESCE($4, age) where id = $5 RETURNING *`,
+      [name, email, password, age, id]
+    )
+    if(result.rows.length===0){
+      return res.status(404).json({ message: "user not found" });
+    }
+    res.status(200).json({status:true, message: "user updated successfully", data: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ message: "error updating user", error: err });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
