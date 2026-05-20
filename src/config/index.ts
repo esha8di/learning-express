d43b66c26dbdc85af490188d0c03bf7d2fc07126
Esha <shuminnahar8di@gmail.com>
2026-05-20 08:17:07 +0600

    done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,16 @@
 import dotenv from "dotenv"
+import { connect } from "http2";
 import path from "path"
 
  dotenv.config(
     {
-        path:path.resolve(process.cwd(),"env")
+        path:path.resolve(process.cwd(),".env")
     }
  )
 
 const config={
-    port:process.env.PORT
+    port:process.env.PORT,
+    connectionString:process.env.CONNECTION_STRING
 }
 
 export default config;
